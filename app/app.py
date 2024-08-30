9a4fce53026ed3277e3a0b78acf6d920c3f578ed
LoskoMiguel <magelposada@gmail.com>
2024-08-30 17:39:28 -0500

    Sistema para validar respuestas

--- app/app.py
+++ app/app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, request, render_template
 
-@app.route("/")
-def index():
-    return "hola mundo"
+app = Flask(__name__)
+
+respuestas = {
+    "respuesta1": "2",
+    "respuesta2": "5"
+}
+
+@app.route("/preguntas", methods=["GET", "POST"])
+def preguntas():
+    puntos = 0
+    if request.method == "POST":    
+        for pregunta, respuesta in respuestas.items():
+            ejercisios = request.form.get(pregunta)
+            if ejercisios == respuesta:
+                puntos += 1
+
+    return render_template("preguntas.html", data=respuestas, puntos=puntos, len=len, ejercisios=ejercisios)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
