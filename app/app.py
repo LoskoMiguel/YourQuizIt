da7788b2adca39caf3ca99876df5885f74766550
LoskoMiguel <magelposada@gmail.com>
2024-09-18 15:37:19 -0500

    Agregue la funcionalidad de que tu puedas seleccioanr el tiempo gramatical

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
+import os
 from flask import Flask, session
 from app.routes.main import main as main_blueprint
 from app.routes.quiz import quiz as quiz_blueprint
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = "loskoalbeiroviveenelbosque"
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 app.register_blueprint(main_blueprint)
 app.register_blueprint(quiz_blueprint)
-
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
